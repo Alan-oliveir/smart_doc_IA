231404ca01386a7571bfdda2891f6c32c1dc7e8e
Alan-oliveir <alandeog@poli.ufrj.br>
2023-10-21 19:41:10 -0300

    modified:   src/App.jsx 	modified:   src/components/Card.jsx 	new file:   src/components/Documentation.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,32 @@
+import { useState } from "react";
 import { Card } from "./components/Card";
 import themes, { language } from "./data";
+import { aiRequest } from "./services/openai.config";
 
 function App() {
-  console.log(themes)
+
+  const [theme, setTheme] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  async function handleClickTheme(message) {
+    setLoading(true);
+    const response = await aiRequest(message);
+    setTheme(response.data.choices[0].text);
+    setLoading(false);
+  }
+
   return (
     <section className="grid place-content-center w-full h-full">
+
       <h1 className="text-amber-400 text-5xl font-bold grid grid-cols-1 py-10 text-center">
         {language}
       </h1>
-      <div className="grid grid-cols-4 gap-10"> 
-        {themes.map((theme, idx) => (
-          <Card key={idx} title={theme.name}/>
-        ))}
-      </div>   
+
+      {theme ? ( <Documentation handdleBackToCard={handdleBackToCard} responseAI={theme}/>) 
+      : ( <div className="grid grid-cols-4 gap-10">
+            {themes.map((theme, idx) => (<Card key={idx} title={theme.name} onClick={() => handleClickTheme(theme.message)}/>))}
+          </div>
+      )}
     </section>
   );
 }
