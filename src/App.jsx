48af205ab6d1de881dcc6a38edde29010f155fcc
Alan-oliveir <alandeog@poli.ufrj.br>
2023-10-21 18:44:00 -0300

    change index.httml, App.jsx and Card.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import themes, { language } from "./data";
 function App() {
   console.log(themes)
   return (
-    <>
-    <h1 className="text-amber-400 text-5xl font-bold grid grid-cols-1 py-10 text-center">
-      {language}
-    </h1>
-    {themes.map((theme, idx) => (
-      <Card key={idx}/>
-    ))}
-    </>
+    <section className="grid place-content-center w-full h-full">
+      <h1 className="text-amber-400 text-5xl font-bold grid grid-cols-1 py-10 text-center">
+        {language}
+      </h1>
+      <div className="grid grid-cols-4 gap-10"> 
+        {themes.map((theme, idx) => (
+          <Card key={idx}/>
+        ))}
+      </div>   
+    </section>
   );
 }
 
