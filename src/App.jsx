7f3ce5ca9c00a90821446a5b5184ae7f5568f21b
Alan-oliveir <alandeog@poli.ufrj.br>
2023-10-21 18:29:24 -0300

    Create cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import themes, {language} from "./data";
+import { Card } from "./components/Card";
+import themes, { language } from "./data";
 
 function App() {
   console.log(themes)
   return (
     <>
-    <h1>{language}</h1>
+    <h1 className="text-amber-400 text-5xl font-bold grid grid-cols-1 py-10 text-center">
+      {language}
+    </h1>
     {themes.map((theme, idx) => (
-      <p key={idx}> {theme.name}</p>
+      <Card key={idx}/>
     ))}
     </>
   );
---
+++ src/components/Card.jsx
@@ -0,0 +1,9 @@
+export function Card() {
+
+    return (
+        <div className="p-4 w-auto h-32 flex justify-center items-center text-center border-2 border-amber-400 cursor-pointer font-bold hover:bg-amber-400 hover:text-white rounded-lg shadow-lg duration-700 text-amber-400">
+
+        <h2 className="card-title">Teste</h2>
+        </div>
+    )
+}
